87164b8dadb2729ca2055af38f11e39d3e9c99c9
R <mikrytis@gmail.com>
2020-10-10 19:53:32 +0300

    multiplayer

---
+++ Assets/AssignReferences.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class AssignReferences : MonoBehaviour
+{
+    public RealtimeAvatarManager manager;
+    public Transform head;
+    public Transform handL;
+    public Transform handR;
+    public Transform headRef;
+    public Transform handLRef;
+    public Transform handRRef;
+    public GameObject root;
+    // Start is called before the first frame update
+    void Start()
+    {
+        root = GameObject.Find("MRTK-Quest_OVRCameraRig(Clone)");
+        headRef = GameObject.Find("CenterEyeAnchor").transform;
+        handLRef = GameObject.Find("LeftControllerAnchor").transform;
+        handRRef = GameObject.Find("RightControllerAnchor").transform;
+    }
+    private void Update()
+    {
+        head.position = headRef.position;
+        head.rotation = headRef.rotation;
+
+        handL.position = handLRef.position;
+        handL.rotation = handLRef.rotation;
+
+        handR.position = handRRef.position;
+        handR.rotation = handRRef.rotation;
+    }
+}
---
+++ Assets/DisableIfLocal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class DisableIfLocal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.root.GetComponent<RealtimeView>().isOwnedLocally)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
